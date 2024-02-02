553eacf3ffa85d1379ee47c0602427b36559f7b4
withado <csleight06@icloud.com>
2024-02-02 15:58:08 -0500

    fix: added create_layer() to __init__()

--- neural_network.py
+++ neural_network.py
@@ -4,10 +4,9 @@ from random import uniform
 class networkLayer() :
     def __init__(self, neuronCount_previousLayer, neuronCount_Current) :
         self.weightCount, self.neuronCount = neuronCount_previousLayer, neuronCount_Current
-
-    def create_layer(self) :
         self.weights = [ [uniform(-1, 1) for i in range(self.weightCount)] for i in range(self.neuronCount)]
-        print(self.weights)
+        self.values = [0 for i in range(self.neuronCount)]
+        print('done')
+
 
 e = networkLayer(6, 6)
-e.create_layer()
