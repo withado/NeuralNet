f60dd34f684d9aa88aa56d46369bed4bc296a5f8
withado <csleight06@icloud.com>
2024-02-02 16:13:23 -0500

    fix: added comments

--- neural_network.py
+++ neural_network.py
@@ -3,15 +3,22 @@ from random import uniform
 
 class networkLayer() :
     def __init__(self, neuronCount_previousLayer, neuronCount_Current) :
+        # initial variables
         self.weightCount, self.neuronCount = neuronCount_previousLayer, neuronCount_Current
+
+        # create arrays for the layer's weights and values
         self.weights = [ [uniform(-1, 1) for i in range(self.weightCount)] for i in range(self.neuronCount)]
         self.values = [0 for i in range(self.neuronCount)]
-        print('done')
 
+    # 1) feed an input (usually previous layer)
     def propagate(self, input) :
+        # 2) go through each neuron
         for neuronIndex, value in enumerate(self.values) :
+            # 3) go through weights of said neuron
             for neuronIndex_input, weight in enumerate(self.weights) :
+                # 4) determine value of neuron (z = sigma_n(input_n * weight_n) + bias_neuron) 
+                # for now, we will just be working with the alteration of the weights
                 self.values[neuronIndex] += input[neuronIndex_input] * weight
 
-
+# Test
 e = networkLayer(6, 6)
